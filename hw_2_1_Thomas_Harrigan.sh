0e433395d4bf552632ae89afed653307efbb9f21
Thomas Harrigan <tjharrigan.dev@gmail.com>
2016-09-26 13:31:40 -0400

    First Commit

---
+++ hw_2_1_Thomas_Harrigan.sh
@@ -0,0 +1,3 @@
+#A bash script that prompts the user for an ID, checks if it is in /etc/passwd.
+#If the ID is in the passwd file, asks the user if this is the entry that should be changed
+#If it is, prompt for new login shell and create a file named passwd.1 which substitues old login shell w/ new one.
